df4da4fadb8fb906ef89a37ae7e76fc342a19b69
saba farzanegan <saba.far.99@gmail.com>
2025-03-02 15:26:44 +0330

    change

--- app/api/auth/[kindeAuth]/route.ts
+++ app/api/auth/[kindeAuth]/route.ts
@@ -1,23 +1,3 @@
-// import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-
-<<<<<<< HEAD
-export const GET = handleAuth();
-
-// import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-// import { cookies } from "next/headers";
-
-// export const GET = async (req: Request, res: string) => {
-//   cookies(); // ensures Next.js properly stores cookies
-//   return handleAuth(req, res); // provide both req and res
-// };
-=======
-// export const GET = handleAuth();
-
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-import { cookies } from "next/headers";
 
-export const GET = async (req: Request, res: string) => {
-  cookies(); // ensures Next.js properly stores cookies
-  return handleAuth(req, res); // provide both req and res
-};
->>>>>>> f351da003900852aad7454298112e27c49fafa63
+export const GET = handleAuth();
