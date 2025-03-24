5b1279f2818443f561dc3d56559edbc01aff8a44
saba farzanegan <saba.far.99@gmail.com>
2025-03-24 22:46:30 +0330

    solve build error

--- components/footer/FooterLinks.tsx
+++ components/footer/FooterLinks.tsx
@@ -12,8 +12,8 @@ function FooterLinks({
       <div className="flex flex-col justify-center text-[14px]">
         <p className=" text-white font-semibold mb-6 mt-3">{title}</p>
         <ul className="list-disc space-y-4 text-neutral-300">
-          {listItems.map((item) => (
-            <li>{item}</li>
+          {listItems.map((item, index) => (
+            <li key={index}>{item}</li>
           ))}
         </ul>
       </div>
